3bfaf05fb5c712b13ff12fb4ebe4e6abaa15af5d
gabriel <gabriel2.alves@usp.br>
2021-10-29 02:40:11 +0200

    graph finished

--- path_check.hs
+++ path_check.hs
@@ -7,15 +7,16 @@
 
 main :: IO()
 
+-- Test case
 path = [(1,2),(2,3),(3,2),(4,3),(4,5)]
 
+-- pathQ return True if a path is found and False otherwise
 pathQ :: [(Int, Int)] -> Int -> Int -> Bool
-pathQ [] a b = (a == b)
 pathQ graph a b
   | a==b = True
   | otherwise =
       -- Evaluates the function for all the edges which arrive on b from y but
-      -- leave from (a,y) 
+      -- leave from (a,y)
       or [pathQ compPath y b | (x,y) <- graph, x == a]
       where
         -- Takes all the edges where a is NOT the source. I.e., constructs a
@@ -28,8 +29,14 @@ pathQ graph a b
 -- compPath = [(2,3),(3,2),(4,3),(4,5)]
 -- or [pathQ [(2,3),(3,2),(4,3),(4,5)] y b, such that x == a], e.g.
 -- If we call [(2,3),(3,2),(4,3),(4,5)] = G1, then
--- or [pathQ G1 y 3 | (x, y) <- graph, x==1], so we are lookinf for paths
--- in G1 which leave from y and arrive on 3
+-- or [pathQ G1 y 3 | (x, y) <- graph, x==1], so we are looking for paths
+-- in G1 which leave from y and arrive on 3. For  (x, y) | x==1 we have (x,y) = (1,2)
+-- so the previous line becomes
+-- or [pathQ G1 2 3]
+-- And this time G2 = compPath = [(1,2),(3,2),(4,3),(4,5)]
+-- so we look for or [pathQ G2 y 3 | (x, y) <- graph, x==2]
+-- and the tuple which satisfies x==2 is only (2,3) so we get
+-- or [pathQ G1 3 3] = or True = True, since a = 3 = b
 
 main =
   do
