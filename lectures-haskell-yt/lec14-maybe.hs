ff4cd69dfcdb8d93294dc89747f4e755cccd9e5f
alves-gabriel <alves.go@protonmail.com>
2022-03-02 07:51:41 -0300

    Maybe data type

--- lectures-haskell-yt/lec14-maybe.hs
+++ lectures-haskell-yt/lec14-maybe.hs
@@ -6,11 +6,43 @@
 --
 -- # Refs
 --  > https://www.youtube.com/watch?v=O0iohEXMCsU
---  >
+--  > https://wiki.haskell.org/Maybe
+--  > https://wiki.haskell.org/Error_vs._Exception
+--  > https://stackoverflow.com/questions/43018227/understanding-maybe-in-haskell
 --
 --  Copy template with: cat template.hs >> "New file"
 
 main :: IO()
+
+-- Here we discuss the Maybe data type
+--
+--  Given a function f x, what should it returns in the case of an error/exception?
+--
+--          f  x
+--         / \
+--        /  \
+--    Result Error (?)
+--
+--  E.g., what is the head of []? Nothing, we get an exception.
+--
+--  data Maybe a = Nothing | Just a
+--
+--  Let's construct a safe division function to investigate this (note the binding =>)
+safediv :: Integral a => a -> a -> Maybe a
+safediv a b =
+  if b == 0 then Nothing else Just $ div a b
+
+--  On GHCI:
+--
+-- *Main> safediv 30 5
+-- Just 6
+-- *Main> safediv 44 0
+-- Nothing
+-- *Main>
+--
+-- We can use import Data.Maybe to borrow some functions for this data type
+--
+--  E.g., isJust :: Maybe a -> Bool, isNothing :: Maybe a -> Bool, or fromJust :: Maybe a -> a
 
 main =
   do
---
+++ lectures-haskell-yt/lec15-io.hs
@@ -0,0 +1,16 @@
+-- # Commands & Topics
+-- Here we learn
+--  > IO Monad
+--
+-- # Hints:
+--
+-- # Refs
+--  > https://www.youtube.com/watch?v=fP0srOQVGB8
+--
+--  Copy template with: cat template.hs >> "New file"
+
+main :: IO()
+
+main =
+  do
+    putStrLn (" " ++ (show ( )))
