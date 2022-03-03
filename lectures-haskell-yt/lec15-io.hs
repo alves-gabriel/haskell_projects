daf50c19eb66638cb1f162c96c95870b8c62a2a3
alves-gabriel <alves.go@protonmail.com>
2022-03-03 06:00:30 -0300

    IO action

--- lectures-haskell-yt/lec15-io.hs
+++ lectures-haskell-yt/lec15-io.hs
@@ -1,6 +1,7 @@
 -- # Commands & Topics
 -- Here we learn
 --  > IO Monad
+--  > Do notation (kinda)
 --
 -- # Hints:
 --
@@ -11,6 +12,23 @@
 
 main :: IO()
 
+-- The do notation performs strict evaluation:
+greet :: IO ()
+greet = do
+  putStrLn "What is your name?"
+  name <- getLine                         -- getLine is an IO action. It reads one line. This is not function application nor definition
+  putStrLn ("Hello, " ++ name ++ ".")
+
+-- How can we loop over IO? Recursion is also the solution here! As an example:
+count :: Int -> Int -> IO ()
+count n m = do
+  putStrLn $ show n
+  if n < m then
+    count (n + 1) m
+  else
+    return ()
+
 main =
   do
-    putStrLn (" " ++ (show ( )))
+    greet
+    count 2 5
